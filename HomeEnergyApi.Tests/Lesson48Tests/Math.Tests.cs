e2ae2ca4b1b6593008c310fbe97df7dd42f3a0ed
Isaac <isaac@knightmoves.org>
2025-09-29 10:06:12 -0500

    test

--- HomeEnergyApi.Tests/Lesson48Tests/Math.Tests.cs
+++ HomeEnergyApi.Tests/Lesson48Tests/Math.Tests.cs
@@ -2,5 +2,27 @@ using HomeEnergyApi.Services;
 
 public class MathTests
 {
+    [Fact]
+    public void ShouldReturnCorrectRomanNumeralFor1()
+    {
+        var result = new MathService().ConvertToRomanNumeral(1);
 
+        Assert.Equal("I", result);
+    }
+
+    [Fact]
+    public void ShouldReturnCorrectRomanNumeralFor483()
+    {
+        var result = new MathService().ConvertToRomanNumeral(483);
+
+        Assert.Equal("CDLXXXIII", result);
+    }
+
+    [Fact]
+    public void ShouldReturnEmptyStringWhenIntegerIsOutOfRange()
+    {
+        var result = new MathService().ConvertToRomanNumeral(-1);
+
+        Assert.Equal("", result);
+    }
 }
